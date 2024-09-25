5115415c0385a9d63b78a9be0395b90e5bf220c1
EricHearst <eric.abt@hearst.com>
2024-09-24 23:34:27 -0400

    style main form

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,20 @@
+// "use client";
+
+import { MainModal } from "./components/MainModal";
+import { WalletConnection } from "./components/WalletConnection";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="">
-        <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-          <a
-            href="/"
-            className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
-          >
-            <span className="fs-4">Simple header</span>
-          </a>
+    <div className="homePage">
+      <main>
+        <header>
+          <span className="">One Man Market Maker</span>
 
-          <ul className="nav nav-pills">
-            <li className="nav-item">
-              <a href="#" className="nav-link">
-                Features
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="#" className="nav-link">
-                Pricing
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="#" className="nav-link">
-                FAQs
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="#" className="nav-link">
-                About
-              </a>
-            </li>
-          </ul>
+          <WalletConnection />
         </header>
+        <MainModal />
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
+      <footer className="">footer</footer>
     </div>
   );
 }
